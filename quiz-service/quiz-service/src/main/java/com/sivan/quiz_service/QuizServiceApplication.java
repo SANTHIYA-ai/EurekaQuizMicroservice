48ea5c74c392c965ee62e15559e52f6b683b645e
SANTHIYA-ai <santhiyarajagiri16922@gmail.com>
2025-10-31 12:19:47 +0530

    Add files via upload

---
+++ quiz-service/quiz-service/src/main/java/com/sivan/quiz_service/QuizServiceApplication.java
@@ -0,0 +1,17 @@
+package com.sivan.quiz_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.sivan.quiz_service.feign")
+
+public class QuizServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizServiceApplication.class, args);
+	}
+
+}
---
+++ quiz-service/quiz-service/src/main/java/com/sivan/quiz_service/model/QuizDto.java
@@ -0,0 +1,11 @@
+package com.sivan.quiz_service.model;
+
+
+import lombok.Data;
+
+@Data
+public class QuizDto {
+    String categoryName;
+    Integer numQuestions;
+    String title;
+}
